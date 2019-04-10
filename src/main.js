f4f133da1e71675a793146b83b993fcb40601af8
Cersei <circegrand@gmail.com>
2019-04-10 15:51:37 +0200

    :card_file_box: add Apollo Client + GraphCool

--- src/main.js
+++ src/main.js
@@ -3,11 +3,37 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import './registerServiceWorker'
+import VueApollo from 'vue-apollo'
+import { HttpLink } from 'apollo-link-http'
+import { InMemoryCache } from 'apollo-cache-inmemory'
+import { ApolloClient } from 'apollo-client'
+
+// HTTP connexion to the API
+const httpLink = new HttpLink({
+  // You should use an absolute URL here
+  uri: 'https://api.graph.cool/simple/v1/cjub9iuxu781f0187a99rywuq'
+})
+
+// Cache implementation
+const cache = new InMemoryCache()
+
+// Create the apollo client
+const apolloClient = new ApolloClient({
+  link: httpLink,
+  cache,
+})
+
+
+
+const apolloProvider = new VueApollo({
+  defaultClient: apolloClient
+})
 
 Vue.config.productionTip = false
 
 new Vue({
   router,
   store,
+  apolloProvider,
   render: h => h(App)
 }).$mount('#app')
