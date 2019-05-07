5d0c9356997c8c47b8794c4326e41a929bd6d81f
Cersei <circegrand@gmail.com>
2019-05-07 12:09:48 +0200

    :lipstick: add scss structure files

--- src/App.vue
+++ src/App.vue
@@ -6,25 +6,9 @@
 </template>
 
 <style lang="scss">
-body, html {
-  margin: 0;
-  padding: 0;
-}
 #app {
-  font-family: 'Montserrat', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+  width: 100%;
+  height: 100%;
   text-align: center;
-  color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
 }
 </style>
