e9709c88711eb018ca5c29a39ac142817c9aeb8f
Cersei <circegrand@gmail.com>
2019-05-08 21:52:22 +0200

    :lipstick: Homepage integration except language select

---
+++ src/components/Nav.vue
@@ -0,0 +1,23 @@
+<template>
+  <nav class="nav">
+    nav
+  </nav>
+</template>
+
+<script>
+
+  export default {
+    name: 'Nav',
+  }
+
+</script>
+
+<style scoped lang="scss">
+
+  .nav {
+    position: fixed;
+    width: 100%;
+    height: 100px;
+  }
+
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,11 +1,26 @@
 <template>
   <div class="home">
-    <label><h1>{{ $t('home.select') }}</h1></label>
-    <select v-model="$i18n.locale" @change="onChangeLang">
-      <option v-for="lang in availableLanguages" :key="`lang-${lang.ident}`" :value="lang.ident">{{lang.lang}}</option>
-    </select>
-    <br>
-    <router-link :to="{ name: 'intro' }"><button>{{ $t('home.start') }}</button></router-link>
+    <div class="home__container">
+      <img class="home__logo" :src="require(`@/assets/img/logo.png`)">
+      <h1 class="home__baseline">{{ $t('home.baseline') }}</h1>
+      <div class="home__select">
+        <span class="select__arrow" @click="onChangeLang"><</span>
+        <ul class="select__languages">
+          <li v-for="(lang, index) in availableLanguages"
+              :class="`select__language ${index === 0 ? 'is-selected' : ''}`"
+              :key="`lang-${lang.ident}`"
+              :data-value="lang.ident">
+            {{lang.lang}}
+          </li>
+        </ul>
+        <span class="select__arrow" @click="onChangeLang">></span>
+      </div>
+      <!--<select class="home__select" v-model="$i18n.locale" @change="onChangeLang">
+        <option v-for="lang in availableLanguages" :key="`lang-${lang.ident}`" :value="lang.ident">{{lang.lang}}</option>
+      </select>-->
+      <router-link class="home__start" :to="{ name: 'intro' }"><button class="home__start--button">{{ $t('home.start') }}</button></router-link>
+      <button class="home__about home__about--button" @click="openAboutPopUp">{{ $t('home.about') }}</button>
+    </div>
   </div>
 </template>
 
@@ -31,11 +46,69 @@ export default {
   methods: {
     onChangeLang () {
       store.commit('setLang', this.$i18n.locale)
+      console.log(this.lang)
+    },
+    openAboutPopUp () {
+      console.log("about pop up is open")
     }
   },
   computed: {
-    hairCut: () => store.getters.getHairCut,
-    lang: () => store.getters.getLang
+    lang: () => store.getters.getLang,
   }
 }
 </script>
+
+<style lang="scss">
+
+  .home {
+    width: 100%;
+    height: 100%;
+    position: relative;
+    @include flexCenter();
+    &__logo {
+      width: 200px;
+    }
+    &__baseline {
+      text-transform: uppercase;
+      font-weight: 300;
+      font-size: toRem(26px);
+      margin: toRem(20px) 0;
+    }
+    &__select {
+      display: block;
+      margin: toRem(50px) auto;
+      .select {
+        &__arrow {
+
+        }
+        &__languages {
+
+        }
+        &__language {
+          opacity: 0;
+          z-index: 0;
+          &.is-selected {
+            opacity: 1;
+            z-index: 1;
+          }
+        }
+      }
+    }
+    &__start {
+      &--button {
+        margin: 0 auto;
+        @include containedButton();
+      }
+    }
+    &__about {
+      position: absolute;
+      bottom: toRem(50px);
+      left: 50%;
+      transform: translateX(-50%);
+      &--button {
+        @include textButton();
+      }
+    }
+  }
+
+</style>
