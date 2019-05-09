a52ee04c5b27eb47f01cf0d30db8729d361b7fbf
Cersei <circegrand@gmail.com>
2019-05-09 11:53:48 +0200

    :lipstick: DONE: about pop-up integration + add all svg icons

--- src/views/About.vue
+++
@@ -1,5 +0,0 @@
-<template>
-  <div class="about">
-    <h1>{{ $t('about.title') }}</h1>
-  </div>
-</template>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -17,15 +17,27 @@
       </div>
       <router-link class="home__start" :to="{ name: 'intro' }"><button class="home__start--button">{{ $t('home.start') }}</button></router-link>
       <button class="home__about home__about--button" @click="openAboutPopUp">{{ $t('home.about') }}</button>
+      <div :class="`home__popup ${isOpen === true ? 'home__popup--open' : ''}`">
+        <button class="home__popup__close" @click="closePopUp">
+          <Icon name="cross" width="50" height="50" stroke="#000000" />
+        </button>
+        <About />
+      </div>
     </div>
   </div>
 </template>
 
 <script>
 import store from '../store/index'
+import About from '@/components/About.vue'
+import Icon from '@/components/icons/Icon.vue'
 
 export default {
   name: 'home',
+  components: {
+    About,
+    Icon
+  },
   data () {
     return {
       availableLanguages: [
@@ -37,7 +49,8 @@ export default {
           ident: 'en',
           lang: 'English'
         }
-      ]
+      ],
+      isOpen: false
     }
   },
   methods: {
@@ -55,7 +68,10 @@ export default {
       selectLanguage.classList.add('is-selected')
     },
     openAboutPopUp () {
-      console.log('About pop-up is open.')
+      this.isOpen = true
+    },
+    closePopUp () {
+      this.isOpen = false
     }
   },
   computed: {
@@ -70,88 +86,150 @@ export default {
     height: 100%;
     position: relative;
     @include flexCenter();
+
     &__logo {
       width: 100px;
     }
+
     &__baseline {
-      margin: 1.25rem 0;
+      margin: 2rem 0;
 
       text-transform: uppercase;
       font-weight: 300;
-      font-size: 1.25rem;
+      font-size: 2rem;
     }
+
     &__select {
-      margin: 3.125rem auto;
+      margin: 5rem auto;
       @include flexCenter();
+
       .select {
+
         &__languages {
           width: 150px;
 
           position: relative;
           @include flexCenter();
         }
+
         &__language {
           position: absolute;
 
           opacity: 0;
           z-index: 0;
 
-          font-size: 1.25rem;
+          font-size: 2rem;
           font-weight: 700;
+
           &.is-selected {
             opacity: 1;
             z-index: 1;
           }
+
         }
+
       }
     }
+
     &__start {
       &--button {
         margin: 0 auto;
-        @include containedButton(0.9375rem 5rem, 1.25rem);
+        @include containedButton(1.5rem 8rem, 2rem);
       }
     }
+
     &__about {
       position: absolute;
-      bottom: 1.25rem;
+      bottom: 2rem;
       left: 50%;
       transform: translateX(-50%);
       &--button {
-        @include textButton(0.875rem);
+        @include textButton(1.4rem);
       }
+    }
+
+    &__popup {
+      width: 100%;
+      height: 100%;
+
+      position: absolute;
+      top: 0;
+      left:0;
+
+      opacity: 0;
+      z-index: -1;
+
+      &__close {
+        position: absolute;
+        top: 1rem;
+        left: 1rem;
+
+        z-index: 10;
+
+        transform: rotate(45deg);
+      }
+
+      &--open {
+        opacity: 1;
+        z-index: 10;
+      }
+
     }
   }
 
   /* ----- TABLET ----- */
   @media (min-width: 768px) and (max-width: 1024px)  {
     .home {
+
       &__logo {
         width: 200px;
       }
+
       &__baseline {
-        font-size: 1.625rem;
+        font-size: 2.6rem;
       }
+
       &__select {
-        margin: 6.25rem auto;
+        margin: 10rem auto;
+
         .select {
+
           &__languages {
             width: 200px;
           }
+
           &__language {
-            font-size: 1.5625rem;
+            font-size: 2.5rem;
           }
+
         }
+
       }
+
       &__start {
+
         &--button {
-          @include containedButton(1.5625rem 8.125rem, 1.75rem);
+          @include containedButton(2.5rem 13rem, 2.8rem);
         }
+
       }
+
       &__about {
-        bottom: 3.125rem;
+        bottom: 5rem;
+
         &--button {
-          @include textButton(1.25rem);
+          @include textButton(2rem);
         }
+
+      }
+
+      &__popup {
+
+        &__close {
+          top: 5rem;
+          left: 5rem;
+        }
+
       }
     }
   }
