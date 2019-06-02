fe0fc8f89f4e849be09683bc04bc0ff8fbad0987
Cersei <circegrand@gmail.com>
2019-06-02 18:24:08 +0200

    :memo: Create sound design config file + add sound design media file

--- src/config/soundDesignSprite.js
+++ src/config/soundDesignSprite.js
@@ -1,5 +1,11 @@
 const soundDesignSprite = {
-
+  analyse_music: [0, 75000], // end: 75000
+  validation: [76000, 1050], // end: 77050
+  analyse_validation: [78000, 10000], // end: 88000
+  timer: [89000, 2100], // end: 91100
+  flash: [92000, 400], // end: 92400
+  background_beach: [94000, 100600], // end: 194600
+  background_park: [200000, 45000] // end: 245000
 }
 
 export default soundDesignSprite
