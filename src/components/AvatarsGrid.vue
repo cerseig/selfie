101c09c9efde8618756b4d45eb204040af0d2356
Cersei <circegrand@gmail.com>
2019-06-06 23:31:27 +0200

    :sparkles: Add animations on HP avatars

--- src/components/AvatarsGrid.vue
+++ src/components/AvatarsGrid.vue
@@ -118,22 +118,6 @@ export default {
 
       }
     }
-
-    /* &.has-been-update {
-
-      .avatars__item:first-child {
-
-        img {
-          position: absolute;
-          top: 50%;
-          left: 50%;
-          height: 100%;
-          width: 100%;
-          transform: translate(-50%, -50%);
-          transition: all 1.5s cubic-bezier(0.445, 0.05, 0.55, 0.95)
-        }
-      }
-    } */
   }
 
 </style>
