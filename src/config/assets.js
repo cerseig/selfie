b72d8663e02c934ebf5a1a155915dcdd6596b5a9
itsleaht <leaztanda@gmail.com>
2019-06-07 17:56:03 +0200

    :sparkles: add assets loader

---
+++ src/config/assets.js
@@ -0,0 +1,32 @@
+const assets = {
+  analysis: {
+    loader: {
+      url: '/img/gifs/loader.gif',
+      type: 'image'
+    }
+  },
+  decors: {
+    mountain: {
+      url: '/img/decors/decor_bath.png',
+      type: 'image'
+    },
+    bath: {
+      url: '/img/decors/decor_beach.png',
+      type: 'image'
+    },
+    eiffel: {
+      url: '/img/decors/decor_eiffel.png',
+      type: 'image'
+    },
+    mountains: {
+      url: '/img/decors/decor_mountains.png',
+      type: 'image'
+    },
+    park: {
+      url: '/img/decors/decor_park.png',
+      type: 'image'
+    }
+  }
+}
+
+export default assets
