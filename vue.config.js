9677bc0ded543de4de7ce331a9f602440152db3f
itsleaht <leaztanda@gmail.com>
2019-06-10 17:38:03 +0200

    :bento: remove useless obj

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production' ? 'https://a-m-y.netlify.com' : './',
+  publicPath: process.env.NODE_ENV === 'production' ? './' : './',
   css: {
     loaderOptions: {
       sass: {
