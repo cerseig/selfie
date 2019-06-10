e2915eeb436a81f340cff72e4aff8d40ebfdc9e8
Cersei <circegrand@gmail.com>
2019-06-10 13:19:57 +0200

    :bug: Remove debug mode

--- src/views/Experience.vue
+++ src/views/Experience.vue
@@ -14,7 +14,7 @@
     <PersonnalisationStep :validateStep="onValidateStep" :isActive="currentStep === STEPS.PERSONNALISATION"  v-if="currentStep < STEPS.POSING" />
     <DecorStep :validateStep="onValidateStep" :isActive="currentStep === STEPS.DECOR" />
 
-    <PosingStep :validateStep="onValidateStep" :isActive="currentStep === STEPS.POSING" :positions="detection.positions" v-if="detection.states.isReady" :detectionManager="detectionManager"/>
+    <PosingStep :validateStep="onValidateStep" :isActive="currentStep === STEPS.POSING" :positions="detection.positions" :detectionManager="detectionManager"/>
 
   </div>
 </template>
