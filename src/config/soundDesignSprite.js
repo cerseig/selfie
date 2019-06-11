f6ae2de11be2142d00c3bd8c1a77b6dbf5073134
Cersei <circegrand@gmail.com>
2019-06-11 22:52:40 +0200

    :bento: Update sound design + add sound design on xp

--- src/config/soundDesignSprite.js
+++ src/config/soundDesignSprite.js
@@ -5,7 +5,8 @@ const soundDesignSprite = {
   success: [84500, 3500], // end: 88000
   timer: [89000, 2100], // end: 91100
   flash: [92000, 400], // end: 92400
-  notification: [246000, 4000] // end: 250000
+  notification: [94000, 4000], // end: 98000
+  backgroundMusic_experience: [100000, 106762] // end : 206762
 }
 
 export default soundDesignSprite
